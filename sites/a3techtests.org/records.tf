44b5659d780f7ec87adde141c57babf77eb652a2
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2025-01-26 18:59:15 -0500

    add a new record

--- sites/a3techtests.org/records.tf
+++ sites/a3techtests.org/records.tf
@@ -9,3 +9,12 @@ resource "cloudflare_record" "root_name" {
   proxied = true
   content = "react-cf-a1c.pages.dev"
 }
+
+resource "cloudflare_record" "staging" {
+  zone_id = cloudflare_zone.azone.id
+  name = "@"
+  type = "CNAME"
+  ttl = 1
+  proxied = true
+  content = "staging.react-cf-a1c.pages.dev"
+}
