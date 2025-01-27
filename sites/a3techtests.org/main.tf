d3380de9a97c566747360f00fdc71f0205e5f874
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2025-01-26 21:22:03 -0500

    add a new record

--- sites/a3techtests.org/main.tf
+++ sites/a3techtests.org/main.tf
@@ -2,9 +2,9 @@
 #   domain = "react-cf-a1c.pages.dev"
 # }
 
-# resource "cloudflare_zone" "azone" {
-#   zone = var.domain_name
-#   #   zone_id = var.zone_id
-#   plan       = "free"
-#   account_id = var.account_id
-# }
+resource "cloudflare_zone" "azone" {
+  zone = var.domain_name
+  #   zone_id = var.zone_id
+  plan       = "free"
+  account_id = var.account_id
+}
