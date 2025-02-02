ed483f1e78f4e16dfdc0acdfb3fecb02d8d90786
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2025-02-01 23:34:58 -0500

    adding a root record

--- sites/a3techtests.org/records.tf
+++ sites/a3techtests.org/records.tf
@@ -1,6 +1,25 @@
 ############################################
 ############### zone records ##############
 ###########################################
+resource "cloudflare_record" "root_cname" {
+  zone_id = data.cloudflare_zone.azone.id
+  name    = "@"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+  content = "react-cf-a1c.pages.dev"
+}
+
+# Example: Manage a DNS record for your Cloudflare Pages site
+resource "cloudflare_record" "pages_cname" {
+  zone_id = data.cloudflare_zone.azone.id
+  name    = "www"
+  type    = "CNAME"
+  ttl = 1
+  proxied = true
+  content   = "react-cf-a1c.pages.dev"
+}
+
 # resource "cloudflare_record" "root_name" {
 #   zone_id = cloudflare_zone.azone.id
 #   name    = "a3techtests.org"
@@ -18,13 +37,3 @@
 #   proxied = true
 #   content = "staging.react-cf-a1c.pages.dev"
 # }
-
-# Example: Manage a DNS record for your Cloudflare Pages site
-resource "cloudflare_record" "pages_cname" {
-  zone_id = data.cloudflare_zone.azone.id
-  name    = "www"
-  type    = "CNAME"
-  ttl = 1
-  proxied = true
-  content   = "react-cf-a1c.pages.dev"
-}
